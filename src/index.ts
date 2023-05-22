603cf96a8c668e572d6954224ad3316255a62e78
so1ve <i@mk1.io>
2023-05-22 12:21:19 +0800

    fix: get exist keys only

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import { klona } from "klona/full";
 
+type NumberKeys<T> = {
+  [K in keyof T]: T[K] extends number ? K : never;
+}[keyof T];
+
 export function createIsomorphicDestructurable<
   T extends Record<string, unknown>,
   A extends readonly any[],
->(obj: T, arr: [...A]): T & A {
+>(obj: T, arr: [...A]): Pick<T & A, keyof T | NumberKeys<A>> {
   const clone = klona(obj);
 
   Object.defineProperty(clone, Symbol.iterator, {
